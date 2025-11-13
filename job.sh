04620a4120c47a959e2964bc1ae07924f7fe96e0
Dafidofff <davidwessels15@gmail.com>
2025-11-13 17:31:48 +0100

    Added first nvidia-run-configs

---
+++ job.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#SBATCH -t 1-00:00:00
+#SBATCH --gres=gpu:4
+#SBATCH --partition=gpu_h100
+#SBATCH --chdir="/home/dwessels2/code/nvSubquadratic-private"
+#SBATCH --output=/home/dwessels2/code/nvSubquadratic-private/outputs/%A.%a/output.txt
+#SBATCH --error=/home/dwessels2/code/nvSubquadratic-private/outputs/%A.%a/error.txt
+#SBATCH --job-name='nvsubq'
+
+# Set wd, activate conda environment.
+source activate nvsubq
+
+# print assigned node
+echo "This job is running on node: $SLURM_NODELIST"
+export HF_TOKEN=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
+
+export PYTHONPATH="."
+python -m experiments.run --config examples/imagenet_classification/ccnn_7_512_hyena.py
+python -m experiments.run --config examples/imagenet_classification/ccnn_7_512_hyena.py
+
+
+
+# python -m experiments.run --config examples/imagenet_diffusion/ccnn_12_768_hyena_rope_qknorm.py
