7fa6e8b9a1acd340577e8589c40d87e3a08bf75c
Olivia Viessmann <olivia.viessmann@gmail.com>
2026-04-03 10:28:16 +0000

    dockerfile adjustments

--- examples/well/v2/active_matter/_base.py
+++ examples/well/v2/active_matter/_base.py
@@ -24,7 +24,7 @@ DATA_DIM = 2
 SPATIAL_RESOLUTION = (256, 256)
 WELL_BASE_PATH = os.environ.get(
     "WELL_DATA_PATH",
-    ".data/the_well/datasets/",
+    "/workspaces/nvSubquadratic-private/data/the_well/datasets/",
 )
 WELL_DATASET_NAME = "active_matter"
 
