6bbbfc5f250537e0b54dca442ef9c4463fe01905
Alireza Moradzadeh <amoradzadeh@nvidia.com>
2026-04-16 08:42:47 -0700

    Update config files and add new submit scripts

--- examples/vit5_imagenet/vit5_hybrid/hybrid_hhha.py
+++ examples/vit5_imagenet/vit5_hybrid/hybrid_hhha.py
@@ -11,12 +11,16 @@ from examples.vit5_imagenet.vit5_hybrid._base_config import (
     build_hybrid_net,
     get_base_config,
 )
+from experiments.default_cfg import ExperimentConfig
 
 
 LAYER_PATTERN = "HHHA" * (NUM_BLOCKS // 4)
 
-config = get_base_config()
-config.compile = True
-config.compile_mode = "max-autotune-no-cudagraphs"
-config.net = build_hybrid_net(layer_pattern=LAYER_PATTERN, patch_size=PATCH_SIZE)
-config.wandb.job_group = "vit5_hybrid"
+
+def get_config() -> ExperimentConfig:
+    config = get_base_config()
+    config.compile = True
+    config.compile_mode = "max-autotune-no-cudagraphs"
+    config.net = build_hybrid_net(layer_pattern=LAYER_PATTERN, patch_size=PATCH_SIZE)
+    config.wandb.job_group = "vit5_hybrid"
+    return config
