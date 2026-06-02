c7f4b7b5d83862a4500c050fa0077e1dacd567a8
Farhad Ramezanghorbani <farhadrgh@users.noreply.github.com>
2026-06-02 16:33:49 -0600

    [DO NOT MERGE] License headers  (#126)
    * update license, contrib, headers
    
    Signed-off-by: Farhad Ramezanghorbani <farhadr@nvidia.com>
    
    * cleanup gitignore
    
    Signed-off-by: Farhad Ramezanghorbani <farhadr@nvidia.com>
    
    * rm todo comment
    
    Signed-off-by: Farhad Ramezanghorbani <farhadr@nvidia.com>
    
    ---------
    
    Signed-off-by: Farhad Ramezanghorbani <farhadr@nvidia.com>

--- examples/vit5_imagenet/v3/ft_ablation/llrd075_ema9999.py
+++ examples/vit5_imagenet/v3/ft_ablation/llrd075_ema9999.py
@@ -1,3 +1,18 @@
+# SPDX-FileCopyrightText: Copyright (c) 2026 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 """FiLM finetuning — LLRD 0.75 + faster EMA decay 0.9999, 10 epochs.
 
 Default EMA decay 0.99996 means the EMA model looks back ~25000 steps.
--- examples/vit5_imagenet/v3/gap_film_regs/r14_f3after_lamb_3e4_wd01.py
+++ examples/vit5_imagenet/v3/gap_film_regs/r14_f3after_lamb_3e4_wd01.py
@@ -1,3 +1,18 @@
+# SPDX-FileCopyrightText: Copyright (c) 2026 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 """14 regs, film3_after, LAMB lr=3e-4 — higher weight decay (0.1).
 
 Same as best LR sweep recipe but doubles wd from 0.05 to 0.1.
