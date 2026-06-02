53ed1b9e1ccbf7cc5dab49908f04632ece3ea78c
Farhad Ramezanghorbani <farhadrgh@users.noreply.github.com>
2026-06-02 16:33:49 -0600

    License headers + Apache 2.0 prep (#126)
    * update license, contrib, headers
    
    Signed-off-by: Farhad Ramezanghorbani <farhadr@nvidia.com>
    
    * cleanup gitignore
    
    Signed-off-by: Farhad Ramezanghorbani <farhadr@nvidia.com>
    
    * rm todo comment
    
    Signed-off-by: Farhad Ramezanghorbani <farhadr@nvidia.com>
    
    ---------
    
    Signed-off-by: Farhad Ramezanghorbani <farhadr@nvidia.com>

--- nvsubquadratic/networks/jit.py
+++ nvsubquadratic/networks/jit.py
@@ -1,8 +1,21 @@
-# SPDX-License-Identifier: MIT
-# Copyright (c) 2023 The JiT authors and contributors.
+# SPDX-FileCopyrightText: Copyright (c) 2026 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-FileCopyrightText: Copyright (c) 2023 The JiT authors and contributors.
+# SPDX-License-Identifier: Apache-2.0
 #
-# This file includes a port of code from the JiT project:
-# https://github.com/LTH14/JiT
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Portions of this file are derived from https://github.com/LTH14/JiT
+# (MIT License). See LICENSE/third_party.txt for the full MIT notice.
 
 """JiT model implementation.
 
